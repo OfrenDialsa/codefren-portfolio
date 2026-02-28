e1b39fc518b32ca3fa40d89eb1044410aa0eb055
OfrenDialsa <ofrendialsa.dev@gmail.com>
2026-02-28 23:08:19 +0700

    chore: modify description

--- src/components/resume-card.tsx
+++ src/components/resume-card.tsx
@@ -63,7 +63,7 @@ export default function ResumeCard({
         <div className="flex-grow ml-4 items-center flex-col group">
           <CardHeader>
             <div className="flex items-center justify-between gap-x-2 text-base">
-              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
+              <h3 className="inline-flex items-center justify-center font-medium leading-none text-xs sm:text-sm">
                 {title}
                 {badges && (
                   <span className="inline-flex gap-x-1">
@@ -98,7 +98,7 @@ export default function ResumeCard({
                 {period}
               </div>
             </div>
-            {subtitle && <div className="font-sans text-xs">{subtitle}</div>}
+            {subtitle && <div className="font-sans text-muted-foreground text-xs">{subtitle}</div>}
           </CardHeader>
           {description && (
             <motion.div
